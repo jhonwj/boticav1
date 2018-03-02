c5a5347c2e53e8ca69dd4362be6d4a92efdb8181
Juan K. Pulido <dxkernel@gmail.com>
2018-03-02 18:00:12 -0500

    add visa y mastercard en cierre de caja

--- views/ve_cierreform.php
+++ views/ve_cierreform.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <title>Cierre de caja</title>
     <?php include_once("linker.php"); ?>
+    <style>
+      #tableCierreDetallePagos td{
+      }
+    </style>
   </head>
   <script type="text/javascript">
     $(document).ready(function(e){
@@ -36,20 +40,31 @@
           console.log(JSON.parse(res));
           var fila = "";
           var sumTotales = 0;
+          var sumEfectivo = 0;
+          var sumVisa = 0;
+          var sumMastercard = 0;
           var sumTotalCredito = 0;
           $.each(JSON.parse(res), function(data, value){
             fila = fila + "<tr><td>"+value.FechaDoc+"</td><td>"+value.TipoDoc+"</td><td>"+value.Serie+"</td><td>"+value.Numero+"</td><td>"+value.Total+"</td></tr>";
-            sumTotales = sumTotales +parseFloat( value.Total);
+            sumTotales += parseFloat( value.Total);
             if (value.EsCredito == 1) {
               sumTotalCredito = sumTotalCredito + parseFloat(value.Total);
             }
+            sumEfectivo += parseFloat(value.Efectivo);
+            sumVisa += parseFloat(value.Visa);
+            sumMastercard += parseFloat(value.Mastercard);
+
           });
           $("#tableCierre tbody").append(fila);
           $("#txtFechaI").val($("#tableCierre tbody").find("tr").eq(0).children("td").eq(0).text());
           $("#txtFechaF").val($("#tableCierre tbody").find("tr:last").eq(0).children("td").eq(0).text());
-          $("#tableCierreDetalle").find("tr").eq(0).children("td").eq(1).text(sumTotales);
-          $("#tableCierreDetalle").find("tr").eq(1).children("td").eq(1).text(sumTotalCredito);
-          $("#tableCierreDetalle").find("tr").eq(2).children("td").eq(1).text(sumTotales - sumTotalCredito);
+          $("#tableCierreDetalle").find("tr").eq(0).children("td").eq(1).text(sumTotales.toFixed(2));
+          $("#tableCierreDetalle").find("tr").eq(1).children("td").eq(1).text(sumTotalCredito.toFixed(2));
+          $("#tableCierreDetalle").find("tr").eq(2).children("td").eq(1).text((sumTotales - sumTotalCredito).toFixed(2));
+
+          $("#tableCierreDetallePagos").find("tr").eq(0).children("td").eq(1).text(sumEfectivo.toFixed(2));
+          $("#tableCierreDetallePagos").find("tr").eq(1).children("td").eq(1).text(sumVisa.toFixed(2));
+          $("#tableCierreDetallePagos").find("tr").eq(2).children("td").eq(1).text(sumMastercard.toFixed(2));
 
         },
         error : function(errorThrown ,XMLHttpRequest, textStatus,){
@@ -66,7 +81,7 @@
     <div class="panel panel-success" style="margin-bottom:0">
       <div class="panel-heading">
         <div class="panel-body">
-          <table id="tableCierreDetalle" class="table table-striped table-bordered">
+          <table id="tableCierreDetalle" class="table table-striped table-bordered" style="width: 50%">
             <tr>
               <td>Salida S/.</td>
               <td></td>
@@ -77,6 +92,20 @@
             </tr>
             <tr>
               <td>Contado</td>
+              <td></td>
+            </tr>
+          </table>
+          <table id="tableCierreDetallePagos" class="table table-striped table-bordered" style="width: 50%">
+            <tr>
+              <td>Efectivo </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Visa</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Mastercard</td>
               <td></td>
             </tr>
           </table>
