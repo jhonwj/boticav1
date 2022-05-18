21273cea4e6a13e5b9db3d3c0e07d355fb4ee0d6
jhonwj <jhonmedranomartinez@gmail.com>
2022-05-17 22:36:21 -0500

    actualizacion

--- imprimir/pagocredito.php
+++ imprimir/pagocredito.php
@@ -59,7 +59,7 @@ if (isset($_GET['idCajaBanco'])) {
 
 <div class="container">
     <!--<div class="center">BOTICA</div>-->
-    <div class="center">FERRETERIA FLEON</div>
+    <div class="center"></div>
     <!--<div class="center">RUC: 20393999544 </div>--><br>
 
     <div class="">COMROBANTE NRO: 001- <?php echo $cajaBanco['IdCajaBanco']; ?></div>
